10cadf519b3971db2fccd23dce59e71cad8a0a4d
Juliana Lucca <lucca.editora@gmail.com>
2023-07-08 14:06:54 -0300

    feat: creating menu navigation functionality

--- js/scripts.js
+++ js/scripts.js
@@ -85,38 +85,76 @@ $(document).ready(function () {
       stop = 1;
     }
   });
-});
 
-// Filtro portfólio
+  // Filtro portfólio
 
-$(".ptf-btn").on("click", function () {
-  let type = $(this).attr("id");
-  let boxes = $(".ptf-item");
+  $(".ptf-btn").on("click", function () {
+    let type = $(this).attr("id");
+    let boxes = $(".ptf-item");
 
-  $(".global-btn").removeClass("active");
-  $(this).addClass("active");
+    $(".global-btn").removeClass("active");
+    $(this).addClass("active");
 
-  if (type == "dsg-btn") {
-    eachBoxes("dsg", boxes);
-  } else if (type == "dev-btn") {
-    eachBoxes("dev", boxes);
-  } else if (type == "seo-btn") {
-    eachBoxes("seo", boxes);
-  } else {
-    eachBoxes("all", boxes);
+    if (type == "dsg-btn") {
+      eachBoxes("dsg", boxes);
+    } else if (type == "dev-btn") {
+      eachBoxes("dev", boxes);
+    } else if (type == "seo-btn") {
+      eachBoxes("seo", boxes);
+    } else {
+      eachBoxes("all", boxes);
+    }
+  });
+
+  function eachBoxes(type, boxes) {
+    if (type == "all") {
+      $(boxes).fadeIn();
+    } else {
+      $(boxes).each(function () {
+        if (!$(this).hasClass(type)) {
+          $(this).fadeOut("slow");
+        } else {
+          $(this).fadeIn();
+        }
+      });
+    }
   }
-});
 
-function eachBoxes(type, boxes) {
-  if (type == "all") {
-    $(boxes).fadeIn();
-  } else {
-    $(boxes).each(function () {
-      if (!$(this).hasClass(type)) {
-        $(this).fadeOut("slow");
-      } else {
-        $(this).fadeIn();
-      }
-    });
-  }
-}
+  // scroll para as seções
+
+  let navBtn = $(".nav-link");
+
+  let bannerSection = $("#mainSlider");
+  let aboutSection = $("#about-area");
+  let servicesSection = $("#services-area");
+  let teamSection = $("#team-area");
+  let portfolioSection = $("#portfolio-area");
+  let contactSection = $("#contact-area");
+
+  let scrollTo = "";
+
+  $(navBtn).click(function () {
+    let btnId = $(this).attr("id");
+
+    if (btnId == "about-menu") {
+      scrollTo = aboutSection;
+    } else if (btnId == "services-menu") {
+      scrollTo = servicesSection;
+    } else if (btnId == "team-menu") {
+      scrollTo = teamSection;
+    } else if (btnId == "portfolio-menu") {
+      scrollTo = portfolioSection;
+    } else if (btnId == "contact-menu") {
+      scrollTo = contactSection;
+    } else {
+      scrollTo = bannerSection;
+    }
+
+    $([document.documentElement, document.body]).animate(
+      {
+        scrollTop: $(scrollTo).offset().top - 70,
+      },
+      1500
+    );
+  });
+});
